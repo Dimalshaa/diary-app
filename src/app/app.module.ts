2f624a17d5bec0159f47a1374391db084f2b7507
Utkarsh Raj <utkarsh3012@gmail.com>
2018-02-20 01:31:30 +0530

    App restructure 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
-import { HttpModule } from "@angular/http";
+import { HttpModule } from '@angular/http';
+import { SuiSidebarModule } from 'ng2-semantic-ui';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { DiaryContentComponent } from './diary-content/diary-content.component';
-import { TimelineComponent } from './diary-content/timeline/timeline.component';
 import { EventsComponent } from './diary-content/events/events.component';
 import { HomeInputComponent } from './home-input/home-input.component';
 import { EventsDataService } from './shared/events-data.service';
-import { EditEventComponent } from './edit-event/edit-event.component';
+import { EditEventComponent } from './diary-content/edit-event/edit-event.component';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full'},
@@ -27,7 +27,6 @@ const appRoutes: Routes = [
     HeaderComponent,
     FooterComponent,
     DiaryContentComponent,
-    TimelineComponent,
     EventsComponent,
     HomeInputComponent,
     EditEventComponent
@@ -36,7 +35,8 @@ const appRoutes: Routes = [
     BrowserModule,
     FormsModule,
     RouterModule.forRoot(appRoutes),
-    HttpModule
+    HttpModule,
+    SuiSidebarModule
   ],
   providers: [EventsDataService],
   bootstrap: [AppComponent]
